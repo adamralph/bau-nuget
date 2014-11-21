1eb6e7a390c4674cf4ed50d9ab4690fb4d3e787e
Adam Ralph <adam@adamralph.com>
2014-11-21 14:25:48 +0200

    refactor: PushFacts

--- src/test/Bau.NuGet.Test.Unit/PushFacts.cs
+++ src/test/Bau.NuGet.Test.Unit/PushFacts.cs
@@ -12,61 +12,66 @@ namespace BauNuGet.Test.Unit
 
     public static class PushFacts
     {
+        // TODO (adamralph): move to a PushFeature acceptance test
         [Fact]
         public static void Pushes()
         {
             // arrange
             var nugetDirectory = NuGetFileFinder.FindFile().Directory;
-            nugetDirectory.Should().NotBeNull();
-            nugetDirectory.Parent.Should().NotBeNull();
-            var nugetPackageFile = nugetDirectory.Parent.EnumerateFiles("*.nupkg").Single();
-            var nugetFakeFolder = new DirectoryInfo("./fake NuGet dot org/"); // keep the slash on, makes a better test
+            nugetDirectory.Should().NotBeNull("NuGet.exe should be in a folder");
+            nugetDirectory.Parent.Should().NotBeNull("NuGet.exe should be in a folder with a parent folder");
+            var nugetPackageFile = nugetDirectory.Parent.EnumerateFiles("*.nupkg").FirstOrDefault();
+            nugetPackageFile.Should().NotBeNull("a .nupkg file should be in the folder above NuGet.exe");
+
+            var pushFolder = new DirectoryInfo("./fake NuGet dot org/"); // keep the slash on, makes a better test
             var task = new NuGetTask();
             task
                 .Push(nugetPackageFile.FullName)
                 .WithWorkingDirectory("./")
-                .WithSource(nugetFakeFolder.FullName)
+                .WithSource(pushFolder.FullName)
                 .WithApiKey("poop")
                 .WithTimeout(123)
                 .WithDisableBuffering(false);
 
-            if (nugetFakeFolder.Exists)
+            if (pushFolder.Exists)
             {
-                nugetFakeFolder.Delete(true);
+                pushFolder.Delete(true);
                 Thread.Sleep(100);
             }
 
-            nugetFakeFolder.Create();
+            pushFolder.Create();
             Thread.Sleep(100);
 
             // act
             task.Execute();
 
             // assert
-            nugetFakeFolder.EnumerateFiles("NuGet.CommandLine.*.nupkg").Should().HaveCount(1);
+            pushFolder.EnumerateFiles(nugetPackageFile.Name).Should().HaveCount(1);
         }
 
+        // TODO (adamralph): move to a NuGetTask component test
         [Fact]
         public static void CreateMultiplePushCommands()
         {
             // arrange
-            var task = new NuGetTask();
-            var fakeDirName = "./fake-dir";
+            var packages = new[] { "package1", "package2" };
+            var workingDirectory = "workingDirectory";
             var apiKey = "poo";
+            var task = new NuGetTask();
 
             // act
             task.Push(
-                new[] { "file1", "file2" },
-                r => r
-                    .WithWorkingDirectory(fakeDirName)
-                    .WithApiKey(apiKey));
+                packages,
+                push => push
+                    .WithApiKey(apiKey)
+                    .WithWorkingDirectory(workingDirectory));
 
             // assert
-            task.Commands.Should().HaveCount(2);
-            task.Commands.All(r => r.WorkingDirectory == fakeDirName).Should().BeTrue();
-            task.Commands.OfType<Push>().All(r => r.ApiKey == apiKey).Should().BeTrue();
-            task.Commands.OfType<Push>().Select(x => x.Package).Should().Contain("file1");
-            task.Commands.OfType<Push>().Select(x => x.Package).Should().Contain("file2");
+            task.Commands.Should().HaveCount(packages.Length);
+            task.Commands.OfType<Push>().Should().HaveCount(packages.Length);
+            task.Commands.OfType<Push>().Select(push => push.Package).Should().BeEquivalentTo(packages);
+            task.Commands.OfType<Push>().Should().OnlyContain(push => push.ApiKey == apiKey);
+            task.Commands.Should().OnlyContain(command => command.WorkingDirectory == workingDirectory);
         }
     }
 }
