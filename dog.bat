c62f8152da77798f763e1694d06a67016fd0c975
Adam Ralph <adam@adamralph.com>
2014-11-22 18:43:37 +0200

    refactor: fix local dogfooding scripts

--- dog.bat
+++ dog.bat
@@ -2,8 +2,8 @@
 
 ECHO [Dog] WARNING: Ensure you have built using baufile.csx before building with this!
 
-ECHO [Dog] Copying Bau.MSBuild from bin\Release...
-xcopy src\Bau.MSBuild\bin\Release\Bau.NuGet.dll packages\Bau.NuGet.0.1.0-beta01\lib\net45\ /Y /Q
+ECHO [Dog] Copying Bau.NuGet from bin\Release...
+xcopy src\Bau.NuGet\bin\Release\Bau.NuGet.dll packages\Bau.NuGet.0.1.0-alpha001\lib\net45\ /Y /Q
 
 ECHO [Dog] Running dog.csx...
 scriptcs dog.csx -- %*
