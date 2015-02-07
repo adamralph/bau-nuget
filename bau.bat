8ab69c3a6ee4a7aeaefa13b2e5b6a1ed24ccfe10
Aaron Dandy <aaron.dandy@live.com>
2015-02-07 12:26:37 -0700

    fixed a bunch of script paths for the 0.13 update

--- bau.bat
+++ bau.bat
@@ -3,4 +3,4 @@ REM TIP: copy bau.bat to your PATH (and delete this line) so you can just type '
 @ECHO OFF
 
 REM you can optionally use the following command in your console instead of running this batch file
-scriptcs baufile.csx -- %*
+scriptcs -script baufile.csx -I -- %*
